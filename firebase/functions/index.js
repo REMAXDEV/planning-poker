8b2acf08ed473bea17e0affc772e9c66866ff957
John Long <johnlong@remax.com>
2020-08-01 11:12:14 -0600

    format

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,108 +1,113 @@
 const functions = require('firebase-functions');
 
-// when show votes buuton is clicked
+// when ShowVotes button is clicked
 exports.showPoints = functions.database.ref('/{room}/showPoints').onWrite((change, context) => {
-  const previousValue = change.before.val();
-  const currentValue = change.after.val();
+    const previousValue = change.before.val();
+    const currentValue = change.after.val();
 
-  if (previousValue != 1 && currentValue == 1) {
-    change.after.ref.parent
-      .child('players')
-      .once('value')
-      .then(snapAfter => {
-        const playersAfter = snapAfter.val();
-        const pointsAfter = playerPoints(playersAfter, false);
-        if (pointsAfter.length >= 2) {
-          pushToElastic(context.params.room, pointsAfter);
-        } else {
-          console.info('== Data invalid, skipped.');
-          return null;
-        }
-      });
-  } else {
-    console.info('== Someone clicked showVote more than one time, skipped.');
-    return null;
-  }
+    if (previousValue != 1 && currentValue == 1) {
+        change.after.ref.parent
+            .once('value')
+            .then(snapAfter => {
+                const roomAfter = snapAfter.val();
+                const pointsAfter = playerPoints(roomAfter.players, false);
+                if (pointsAfter.length >= 2) {
+                    return pushToElastic(context.params.room, pointsAfter, getSessionTime(roomAfter));
+                } else {
+                    console.info('== Data invalid, skipped.');
+                    return null;
+                }
+            });
+    } else {
+        console.info('== Someone clicked ShowVotes more than one time, skipped.');
+        return null;
+    }
 });
 
 // when everyone voted
 exports.everyoneVoted = functions.database.ref('/{room}/players/{playerName}/point').onWrite((change, context) => {
-  const previousValue = change.before.val();
-  const currentValue = change.after.val();
+    const previousValue = change.before.val();
+    const currentValue = change.after.val();
 
-  if (previousValue <= 0 && currentValue > 0) {
-    change.after.ref.parent.parent.once('value').then(snapAfter => {
-      const playersAfter = snapAfter.val();
-      const pointsAfter = playerPoints(playersAfter, true);
-      if (pointsAfter.length >= 2) {
-        pushToElastic(context.params.room, pointsAfter);
-      } else {
-        console.info('== Wait, Some one did not vote.');
+    if (previousValue <= 0 && currentValue > 0) {
+        change.after.ref.parent.parent.parent.once('value').then(snapAfter => {
+            const roomAfter = snapAfter.val();
+            const pointsAfter = playerPoints(roomAfter.players, true);
+            if (pointsAfter.length >= 2) {
+                return pushToElastic(context.params.room, pointsAfter, getSessionTime(roomAfter));
+            } else {
+                console.info('== Wait, some one did not vote.');
+                return null;
+            }
+        });
+    } else {
+        console.info('== Someone changed the point, skipped.');
         return null;
-      }
-    });
-  } else {
-    console.info('== Someone changed the point, skipped.');
-    return null;
-  }
+    }
 });
 
-/* ============= */
+/* ====== helper functions ====== */
 
-function pushToElastic(roomName, points) {
-  const { Client } = require('@elastic/elasticsearch');
-  const client = new Client({
-    node: functions.config().elastic.host,
-    maxRetries: 1,
-    requestTimeout: 1000,
-    auth: {
-      username: functions.config().elastic.user,
-      password: functions.config().elastic.pwd,
-    },
-  });
-  // time
-  const time = new Date().toISOString();
-  // push
-  client.index(
-    {
-      index: 'planning-poker',
-      id: roomName.replace(/\W/g, '') + '@' + time,
-      body: {
-        room: roomName,
-        time: time,
-        points: shuffle(points),
-      },
-    },
-    function(err, resp, status) {
-      if (err) {
-        console.error('== ERROR - HTTP ' + resp.statusCode + ' ' + resp.body.error.reason);
-      } else {
-        console.info('== SUCCESS - HTTP ' + resp.statusCode);
-      }
-    }
-  );
+// should return a promise
+function pushToElastic(roomName, points, sessionTime) {
+    // client
+    const { Client } = require('@elastic/elasticsearch');
+    const client = new Client({
+        node: functions.config().elastic.host,
+        maxRetries: 1,
+        requestTimeout: 1000,
+        auth: {
+            username: functions.config().elastic.user,
+            password: functions.config().elastic.pwd,
+        },
+    });
+    // payload
+    const payload = {
+        index: 'planning-poker',
+        id: roomName.replace(/\W/g, '') + '@' + sessionTime,
+        body: {
+            room: roomName,
+            time: sessionTime,
+            points: shuffle(points),
+        },
+    };
+    // request
+    return client
+        .index(payload)
+        .then(result => {
+            console.info('== SUCCESS : HTTP ' + result.statusCode);
+            return true;
+        })
+        .catch(err => {
+            console.error('== ERROR : HTTP ' + err.statusCode);
+            return false;
+        });
 }
 
 function shuffle(arr) {
-  let j, x, i;
-  for (i = arr.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    x = arr[i];
-    arr[i] = arr[j];
-    arr[j] = x;
-  }
-  return arr;
+    let j, x, i;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = arr[i];
+        arr[i] = arr[j];
+        arr[j] = x;
+    }
+    return arr;
 }
 
 function playerPoints(players, validate) {
-  const points = [];
-  for (const key in players) {
-    if (players[key].point > 0) {
-      points.push(players[key].point);
+    const points = [];
+    for (const key in players) {
+        if (players[key].point > 0) {
+            points.push(players[key].point);
+        }
+        if (players[key].point == 0 && validate) {
+            return [];
+        }
     }
-    if (players[key].point == 0 && validate) {
-      return [];
-    }
-  }
-  return points;
+    return points;
+}
+
+function getSessionTime(room) {
+    return room.time ? room.time : new Date().toISOString();
 }
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -46,6 +46,7 @@ class FirebaseService {
       .then((snapshot: firebase.database.DataSnapshot) => {
         const res = snapshot.val();
         const room: Room = {
+          time: new Date().toISOString(),
           showPoints: 0,
           players: {},
         };
