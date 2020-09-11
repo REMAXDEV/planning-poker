7c6b0491e88b5d375c0567e90572db8b62ad07a0
Nancy Moyers <nancymfsu@gmail.com>
2020-09-11 13:06:42 -0600

    Timed Voting Mode

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import router from './router';
 import store from './store';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCheckCircle, faEllipsisH, faWifi, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle, faEllipsisH, faWifi, faUserCircle, faThumbsUp, faCog } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 library.add(faUserCircle);
 library.add(faCheckCircle);
 library.add(faEllipsisH);
+library.add(faThumbsUp);
 library.add(faWifi);
+library.add(faCog);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
